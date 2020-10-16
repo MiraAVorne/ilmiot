bf72ae4daf4a543482e6dc43128ea1f15642386e
MiraAVorne <61496264+MiraAVorne@users.noreply.github.com>
2020-10-16 10:22:17 +0300

    Create harjoituksia.cpp

---
+++ harjoituksia.cpp
@@ -0,0 +1,25 @@
+// kommenttia yhdellä rivillä
+/*
+kommentteja, 1. rivi
+kommentteja, 2. rivi
+*/
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   cout << "Hello World"  
+        << endl; 
+        
+   int ika = 43;
+   cout << ika
+        << endl;
+    
+    const float pii = 3.141592;
+    cout << pii
+         << endl;
+
+    double pallon_sade, pallon_ala, pallon_tilavuus;
+      
+}
